abf7129537531cee766ab54a69a8e7446355ffa8
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-28 19:15:58 +0200

    example: use span instead of a tags, amend per latest module changes

--- example/index.js
+++ example/index.js
@@ -1,27 +1,20 @@
-$(function() {
+$( function() {
   var timer;
   var delay = 3000;
 
-  if (window !== window.top) {
-    $('html').addClass('is-iframe');
-  }
-
-  $(window).on('load', function() {
-    $('html').removeClass('no-js');
-
-    $('ul').picknick({
-      selector: 'a',
-      active: 'is-active'
-    }).on('pick.nick.d', function (e) {
-      if (timer) {
-        clearTimeout(timer);
+  $( window ).on( "load", function() {
+    $( "ul" ).picknick( {
+      selector: "span"
+    } ).on( "pick.nick", function() {
+      if ( timer ) {
+        clearTimeout( timer );
       }
 
-      timer = setTimeout(function() {
-        $('ul').data('picknick').next();
-      }, delay);
-    }).delay(delay * 0.75).queue(function() {
-        $(this).trigger('pick.next');
-    });
-  });
-});
+      timer = setTimeout( function() {
+        $( "ul" ).data( "picknick" ).next();
+      }, delay );
+    } ).delay( delay * 0.75 ).queue( function() {
+        $( this ).trigger( "pick.next" );
+    } );
+  } );
+} );
